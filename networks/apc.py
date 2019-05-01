12136952fff304b63332ef25e17855232dca1469
Dmitriy <d.danevskyi@gmail.com>
2019-05-01 21:01:55 +0300

    Add apc implementation

---
+++ networks/apc.py
@@ -0,0 +1,360 @@
+import os
+import math
+import itertools
+from collections import defaultdict, OrderedDict, deque
+
+from tqdm import tqdm
+import numpy as np
+import torch
+import torch.nn as nn
+import torchvision.utils
+from tensorboardX import SummaryWriter
+from torch.nn.functional import binary_cross_entropy_with_logits
+
+from ops.training import OPTIMIZERS, make_scheduler, make_step
+from networks.losses import binary_cross_entropy, focal_loss, lsep_loss
+from ops.utils import plot_projection
+
+
+class APCModel(nn.Module):
+
+    def __init__(self, experiment, device="cuda"):
+        super().__init__()
+
+        self.device = device
+
+        self.experiment = experiment
+        self.config = experiment.config
+
+        self.input_norm = nn.LayerNorm(
+            (self.config.data._input_dim,), elementwise_affine=False)
+
+        self.rnn = nn.LSTM(
+            self.config.data._input_dim, self.config.network.rnn_size,
+            num_layers=self.config.network.rnn_layers,
+            batch_first=True
+        )
+
+        self.output_norm = nn.LayerNorm((
+            self.config.network.rnn_size * self.config.network.rnn_layers,))
+
+        self.prediction_transforms = torch.nn.ModuleList([
+            torch.nn.Sequential(
+                torch.nn.Linear(
+                    self.config.network.rnn_size * self.config.network.rnn_layers,
+                    self.config.data._input_dim)
+            )
+            for steps in range(self.config.network.prediction_steps)
+        ])
+
+        self.to(self.device)
+
+    def forward(self, signal):
+
+        # signal = signal.permute(0, 2, 1)
+        signal = self.input_norm(signal)
+        # signal = signal.permute(0, 2, 1)
+
+        output, state = self.rnn(signal)
+        output = self.output_norm(output)
+
+        losses = []
+        predictions = []
+
+        for step, affine in enumerate(self.prediction_transforms, start=1):
+
+            shifted_output = output[:, :-step, :]
+            shifted_signal = signal.detach()[:, step:, :]
+
+            prediction = affine(shifted_output)
+            predictions.append(prediction)
+
+            loss = torch.abs(shifted_signal - prediction)
+            loss = loss.sum(-1)
+            loss = loss.mean()
+
+            losses.append(loss)
+
+        r = dict(
+            losses=losses,
+            output=output,
+            predictions=predictions
+        )
+
+        return r
+
+    def add_scalar_summaries(
+        self, losses, writer, global_step):
+
+        # scalars
+        for k, loss in enumerate(losses, start=1):
+            writer.add_scalar("loss_{k}".format(k=k), loss, global_step)
+
+    def add_image_summaries(
+        self, signal, output, predictions, global_step, writer, to_plot=8):
+
+        if len(signal) > to_plot:
+            signal = signal[:to_plot]
+            output = output[:to_plot]
+            predictions = [p[:to_plot] for p in predictions]
+
+        # signal
+        image_grid = torchvision.utils.make_grid(
+            signal.data.cpu().unsqueeze(1),
+            normalize=True, scale_each=True
+        )
+        writer.add_image("signal", image_grid, global_step)
+        # output
+        image_grid = torchvision.utils.make_grid(
+            output.data.cpu().unsqueeze(1),
+            normalize=True, scale_each=True
+        )
+        writer.add_image("output", image_grid, global_step)
+
+        for k, p in enumerate(predictions, start=1):
+            image_grid = torchvision.utils.make_grid(
+                p.data.cpu().unsqueeze(1),
+                normalize=True, scale_each=True
+            )
+            writer.add_image(
+                "prediction_{k}".format(k=k), image_grid, global_step)
+
+    def add_projection_summary(self, image, global_step, writer, name="projection"):
+        writer.add_image(name, image.transpose(2, 0, 1), global_step)
+
+    def train_epoch(self, train_loader,
+                    epoch, log_interval, write_summary=True):
+
+        self.train()
+
+        print(
+            "\n" + " " * 10 + "****** Epoch {epoch} ******\n"
+            .format(epoch=epoch)
+        )
+
+        history = deque(maxlen=30)
+
+        self.optimizer.zero_grad()
+        accumulated_loss = 0
+
+        with tqdm(total=len(train_loader), ncols=80) as pb:
+
+            for batch_idx, sample in enumerate(train_loader):
+
+                self.global_step += 1
+
+                make_step(self.scheduler, step=self.global_step)
+
+                signal, labels = (
+                    sample["signal"].to(self.device),
+                    sample["labels"].to(self.device)
+                )
+
+                outputs = self(signal)
+
+                losses = outputs["losses"]
+
+                loss = (
+                    sum(losses)
+                ) / self.config.train.accumulation_steps
+
+                loss.backward()
+                accumulated_loss += loss
+
+                if batch_idx % self.config.train.accumulation_steps == 0:
+                    self.optimizer.step()
+                    accumulated_loss = 0
+                    self.optimizer.zero_grad()
+
+                history.append(loss.item())
+
+                pb.update()
+                pb.set_description(
+                    "Loss: {:.4f}".format(
+                        np.mean(history)))
+
+                if batch_idx % log_interval == 0:
+                    self.add_scalar_summaries(
+                        [loss.item() for loss in losses],
+                        self.train_writer, self.global_step)
+
+                if batch_idx == 0:
+                    self.add_image_summaries(
+                        signal,
+                        outputs["output"],
+                        outputs["predictions"],
+                        self.global_step, self.train_writer)
+
+    def evaluate(self, loader, verbose=False, write_summary=False, epoch=None):
+
+        self.eval()
+
+        valid_losses = [0 for _ in range(self.config.network.prediction_steps)]
+
+        all_outputs = []
+        all_labels = []
+
+        with torch.no_grad():
+            for batch_idx, sample in enumerate(loader):
+
+                signal, labels = (
+                    sample["signal"].to(self.device),
+                    sample["labels"].to(self.device)
+                )
+
+                outputs = self(signal)
+
+                losses = outputs["losses"]
+
+                multiplier = len(signal) / len(loader.dataset)
+
+                for k, loss in enumerate(losses):
+                    valid_losses[k] += loss.item() * multiplier
+
+                all_outputs.extend(
+                    outputs["output"].permute(0, 2, 1).data.cpu().numpy())
+                all_labels.extend(labels.data.cpu().numpy())
+
+        valid_loss = sum(valid_losses)
+
+        all_labels = np.array(all_labels)
+
+        if write_summary:
+            self.add_scalar_summaries(
+                valid_losses,
+                writer=self.valid_writer, global_step=self.global_step
+            )
+            if epoch % self.config.train._proj_interval == 0:
+                self.add_projection_summary(
+                    plot_projection(
+                        all_outputs, all_labels, frames_per_example=5, newline=True),
+                    writer=self.valid_writer, global_step=self.global_step,
+                    name="projection_output")
+
+        if verbose:
+            print("\nValidation loss: {:.4f}".format(valid_loss))
+
+        return -valid_loss
+
+    def validation(self, valid_loader, epoch):
+        return self.evaluate(
+            valid_loader,
+            verbose=True, write_summary=True, epoch=epoch)
+
+    def predict(self, loader):
+
+        self.eval()
+
+        all_class_probs = []
+
+        with torch.no_grad():
+            for sample in loader:
+
+                signal = sample["signal"].to(self.device)
+
+                outputs = self(signal)
+
+                class_logits = outputs["class_logits"].squeeze()
+
+                class_probs = torch.sigmoid(class_logits).data.cpu().numpy()
+                all_class_probs.extend(class_probs)
+
+        all_class_probs = np.asarray(all_class_probs)
+
+        return all_class_probs
+
+    def fit_validate(self, train_loader, valid_loader, epochs, fold,
+                     log_interval=25):
+
+
+        self.experiment.register_directory("summaries")
+        self.train_writer = SummaryWriter(
+            log_dir=os.path.join(
+                self.experiment.summaries,
+                "fold_{}".format(fold),
+                "train"
+            )
+        )
+        self.valid_writer = SummaryWriter(
+            log_dir=os.path.join(
+                self.experiment.summaries,
+                "fold_{}".format(fold),
+                "valid"
+            )
+        )
+
+        os.makedirs(
+            os.path.join(
+                self.experiment.checkpoints,
+                "fold_{}".format(fold)),
+            exist_ok=True
+        )
+
+        self.global_step = 0
+        self.make_optimizer(max_steps=len(train_loader) * epochs)
+
+        scores = []
+        best_score = 0
+
+        for epoch in range(epochs):
+
+            make_step(self.scheduler, epoch=epoch)
+
+            if epoch == self.config.train.switch_off_augmentations_on:
+                train_loader.dataset.transform.switch_off_augmentations()
+
+            self.train_epoch(
+                train_loader, epoch,
+                log_interval, write_summary=True
+            )
+            validation_score = self.validation(valid_loader, epoch)
+            scores.append(validation_score)
+
+            if epoch % self.config.train._save_every == 0:
+                print("\nSaving model on epoch", epoch)
+                torch.save(
+                    self.state_dict(),
+                    os.path.join(
+                        self.experiment.checkpoints,
+                        "fold_{}".format(fold),
+                        "model_on_epoch_{}.pth".format(epoch)
+                    )
+                )
+
+            if validation_score > best_score:
+                torch.save(
+                    self.state_dict(),
+                    os.path.join(
+                        self.experiment.checkpoints,
+                        "fold_{}".format(fold),
+                        "best_model.pth"
+                    )
+                )
+                best_score = validation_score
+
+        return scores
+
+    def make_optimizer(self, max_steps):
+
+        optimizer = OPTIMIZERS[self.config.train.optimizer]
+        optimizer = optimizer(
+            self.parameters(),
+            self.config.train.learning_rate,
+            weight_decay=self.config.train.weight_decay
+        )
+        self.optimizer = optimizer
+        self.scheduler = make_scheduler(
+            self.config.train.scheduler, max_steps=max_steps)(optimizer)
+
+    def load_best_model(self, fold):
+
+        self.load_state_dict(
+            torch.load(
+                os.path.join(
+                    self.experiment.checkpoints,
+                    "fold_{}".format(fold),
+                    "best_model.pth"
+                )
+            )
+        )
+
---
+++ train_apc.py
@@ -0,0 +1,278 @@
+import os
+import gc
+import argparse
+import json
+import math
+from functools import partial
+
+import pandas as pd
+import numpy as np
+import torch
+from mag.experiment import Experiment
+import mag
+from sklearn.model_selection import train_test_split
+
+from ops.utils import load_json, get_class_names_from_classmap
+from datasets.sound_dataset import SoundDataset
+from networks.apc import APCModel
+from ops.folds import train_validation_data
+from ops.transforms import (
+    Compose, DropFields, LoadAudio,
+    AudioFeatures, MapLabels, RenameFields,
+    MixUp, SampleSegment, SampleLongAudio,
+    AudioAugmentation)
+from ops.padding import make_collate_fn
+
+torch.manual_seed(42)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed_all(42)
+
+mag.use_custom_separator("-")
+
+parser = argparse.ArgumentParser(
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter
+)
+
+parser.add_argument(
+    "--train_df", required=True, type=str,
+    help="path to train dataframe"
+)
+parser.add_argument(
+    "--train_data_dir", required=True, type=str,
+    help="path to train data"
+)
+parser.add_argument(
+    "--classmap", required=True, type=str,
+    help="path to class map json"
+)
+parser.add_argument(
+    "--log_interval", default=10, type=int,
+    help="how frequently to log batch metrics"
+    "in terms of processed batches"
+)
+parser.add_argument(
+    "--proj_interval", default=10, type=int,
+    help="how frequently to make projection in terms of epochs"
+)
+parser.add_argument(
+    "--batch_size", type=int, default=64,
+    help="minibatch size"
+)
+parser.add_argument(
+    "--max_audio_length", type=int, default=10,
+    help="max audio length in seconds. For longer clips are sampled"
+)
+parser.add_argument(
+    "--lr", default=0.01, type=float,
+    help="starting learning rate"
+)
+parser.add_argument(
+    "--max_samples", type=int,
+    help="maximum number of samples to use"
+)
+parser.add_argument(
+    "--epochs", default=100, type=int,
+    help="number of epochs to train"
+)
+parser.add_argument(
+    "--scheduler", type=str, default="steplr_1_0.5",
+    help="scheduler type",
+)
+parser.add_argument(
+    "--accumulation_steps", type=int, default=1,
+    help="number of gradient accumulation steps",
+)
+parser.add_argument(
+    "--save_every", type=int, default=1,
+    help="how frequently to save a model",
+)
+parser.add_argument(
+    "--device", type=str, required=True,
+    help="whether to train on cuda or cpu",
+    choices=("cuda", "cpu")
+)
+parser.add_argument(
+    "--rnn_size", type=int, default=128,
+    help="rnn size"
+)
+parser.add_argument(
+    "--rnn_layers", type=int, default=2,
+    help="number of rnn layers"
+)
+parser.add_argument(
+    "--prediction_steps", type=int, default=10,
+    help="how many steps to predict in the future"
+)
+parser.add_argument(
+    "--weight_decay", type=float, default=1e-5,
+    help="weight decay"
+)
+parser.add_argument(
+    "--p_aug", type=float, default=0.0,
+    help="probability of audio augmentation"
+)
+parser.add_argument(
+    "--switch_off_augmentations_on", type=int, default=20,
+    help="on which epoch to remove augmentations"
+)
+parser.add_argument(
+    "--features", type=str, required=True,
+    help="feature descriptor"
+)
+parser.add_argument(
+    "--optimizer", type=str, required=True,
+    help="which optimizer to use",
+    choices=("adam", "momentum")
+)
+parser.add_argument(
+    "--folds", type=int, required=True, nargs="+",
+    help="which folds to use"
+)
+parser.add_argument(
+    "--n_folds", type=int, default=4,
+    help="number of folds"
+)
+parser.add_argument(
+    "--kfold_seed", type=int, default=42,
+    help="kfold seed"
+)
+parser.add_argument(
+    "--num_workers", type=int, default=4,
+    help="number of workers for data loader",
+)
+parser.add_argument(
+    "--label", type=str, default="apc",
+    help="optional label",
+)
+args = parser.parse_args()
+
+class_map = load_json(args.classmap)
+
+audio_transform = AudioFeatures(args.features)
+
+with Experiment({
+    "network": {
+        "prediction_steps": args.prediction_steps,
+        "rnn_size": args.rnn_size,
+        "rnn_layers": args.rnn_layers
+    },
+    "data": {
+        "features": args.features,
+        "_n_folds": args.n_folds,
+        "_kfold_seed": args.kfold_seed,
+        "_input_dim": audio_transform.n_features,
+        "p_aug": args.p_aug,
+        "max_audio_length": args.max_audio_length
+    },
+    "train": {
+        "_proj_interval": args.proj_interval,
+        "accumulation_steps": args.accumulation_steps,
+        "batch_size": args.batch_size,
+        "learning_rate": args.lr,
+        "scheduler": args.scheduler,
+        "optimizer": args.optimizer,
+        "epochs": args.epochs,
+        "_save_every": args.save_every,
+        "weight_decay": args.weight_decay,
+        "switch_off_augmentations_on": args.switch_off_augmentations_on
+    },
+    "label": args.label
+}) as experiment:
+
+    config = experiment.config
+    print()
+    print("     ////// CONFIG //////")
+    print(experiment.config)
+
+    train_df = pd.read_csv(args.train_df)
+
+    if args.max_samples:
+        train_df = train_df.sample(args.max_samples).reset_index(drop=True)
+
+    splits = list(train_validation_data(
+        train_df.fname, train_df.labels,
+        config.data._n_folds, config.data._kfold_seed))
+
+    for fold in args.folds:
+
+        print("\n\n   -----  Fold {}\n".format(fold))
+
+        train, valid = splits[fold]
+
+        loader_kwargs = (
+            {"num_workers": args.num_workers, "pin_memory": True}
+            if torch.cuda.is_available() else {})
+
+        experiment.register_directory("checkpoints")
+        experiment.register_directory("predictions")
+
+        train_loader = torch.utils.data.DataLoader(
+            SoundDataset(
+                audio_files=[
+                    os.path.join(args.train_data_dir, fname)
+                    for fname in train_df.fname.values[train]],
+                labels=[
+                    item.split(",") for item in
+                    train_df.labels.values[train]],
+                transform=Compose([
+                    LoadAudio(),
+                    MapLabels(class_map=class_map),
+                    SampleLongAudio(max_length=args.max_audio_length),
+                    AudioAugmentation(p=args.p_aug),
+                    audio_transform,
+                    DropFields(("audio", "filename", "sr")),
+                ])
+            ),
+            shuffle=True,
+            drop_last=True,
+            batch_size=config.train.batch_size,
+            collate_fn=make_collate_fn({"signal": audio_transform.padding_value}),
+            **loader_kwargs
+        )
+
+        valid_loader = torch.utils.data.DataLoader(
+            SoundDataset(
+                audio_files=[
+                    os.path.join(args.train_data_dir, fname)
+                    for fname in train_df.fname.values[valid]],
+                labels=[
+                    item.split(",") for item in
+                    train_df.labels.values[valid]],
+                transform=Compose([
+                    LoadAudio(),
+                    MapLabels(class_map=class_map),
+                    SampleLongAudio(max_length=args.max_audio_length),
+                    audio_transform,
+                    DropFields(("audio", "filename", "sr")),
+                ])
+            ),
+            shuffle=False,
+            batch_size=config.train.batch_size,
+            collate_fn=make_collate_fn({"signal": audio_transform.padding_value}),
+            **loader_kwargs
+        )
+
+        model = APCModel(experiment, device=args.device)
+
+        scores = model.fit_validate(
+            train_loader, valid_loader,
+            epochs=experiment.config.train.epochs, fold=fold,
+            log_interval=args.log_interval
+        )
+
+        best_metric = max(scores)
+        experiment.register_result("fold{}.metric".format(fold), best_metric)
+
+        torch.save(
+            model.state_dict(),
+            os.path.join(
+                experiment.checkpoints,
+                "fold_{}".format(fold),
+                "final_model.pth")
+        )
+
+        # predictions
+        model.load_best_model(fold)
+
+        if args.device == "cuda":
+            torch.cuda.empty_cache()
